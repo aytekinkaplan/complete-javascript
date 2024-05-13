48bbcd4db2483007c3748668f9be0aef88219cb7
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-13 07:47:19 +0300

    New commit

---
+++ jsstudy001/class001.ts
@@ -0,0 +1,57 @@
+let greeting: string = "Hello";
+console.log(greeting);
+
+// Linking a JavaScript File
+
+let js: string = "Amazing";
+console.log(js);
+
+console.log(40 + 8 + 23 - 10);
+
+console.log("Hello World");
+
+console.log(23);
+
+let firstName: string = "Jonas";
+console.log(firstName);
+
+let lastName: string = "Schmedtmann";
+console.log(lastName);
+
+let age: number = 2037 - 1991;
+console.log(age);
+
+let myAge: number = 30;
+console.log(myAge);
+
+let currentYear: number = 2022;
+console.log(currentYear);
+
+let choice:boolean = true;
+console.log(choice);
+
+let truth: boolean = false;
+console.log(truth);
+
+console.log(typeof age);
+
+console.log(typeof choice);
+
+// Basic Operators
+// Math operators
+
+//let numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+//console.log(numNeighbours + 1);
+
+console.log(40 > 34);
+const now: number = 2037;
+const ageJonas: number = now - 1991;
+const ageSarah: number = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+console.log(firstName + " " + lastName);
+
+
+
+
