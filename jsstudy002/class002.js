6a0c80eab755c814d457db615f9ec388428872dd
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-14 22:53:51 +0300

    New commit

--- jsstudy002/class002.js
+++ jsstudy002/class002.js
@@ -1,8 +1,8 @@
 var calcAge = function (currentYear, birthYear) { return currentYear - birthYear; };
 console.log(calcAge(2024, 2016));
 var sayMyName = function (firstName) { return "My name is ".concat(firstName); };
-console.log(sayMyName("Ahmed Yusuf"));
+console.log(sayMyName("Michael"));
 var mySelf = function (firstName, lastName, birthYear, currentYear, favoriteColor, favoriteFood, favoriteComputerGame) {
     return "My name is ".concat(firstName, " ").concat(lastName, ". I am ").concat(currentYear - birthYear, " years old. My favorite color is ").concat(favoriteColor, ",and also my favorite food is ").concat(favoriteFood, " and my favorite computer game is ").concat(favoriteComputerGame, "!");
 };
-console.log(mySelf("Ahmed Yusuf", "Kaplan", 2016, 2024, "Red", "Nodle", "Brawl Stars"));
+console.log(mySelf("Michael", "Callaway", 2016, 2024, "Red", "Nodle", "Brawl Stars"));
--- jsstudy002/class002.ts
+++ jsstudy002/class002.ts
@@ -3,10 +3,10 @@ const calcAge = (currentYear: number, birthYear: number) => currentYear - birthY
 console.log(calcAge(2024, 2016));
 
 const sayMyName = (firstName: string) => `My name is ${firstName}`;
-console.log(sayMyName("Ahmed Yusuf"));
+console.log(sayMyName("Michael"));
 
 const mySelf = (firstName: string, lastName: string, birthYear: number, currentYear: number, favoriteColor: string, favoriteFood: string, favoriteComputerGame: string): string => {
     return `My name is ${firstName} ${lastName}. I am ${currentYear - birthYear} years old. My favorite color is ${favoriteColor},and also my favorite food is ${favoriteFood} and my favorite computer game is ${favoriteComputerGame}!`;
 }
 
-console.log(mySelf("Ahmed Yusuf", "Kaplan", 2016, 2024, "Red", "Nodle", "Brawl Stars"));
+console.log(mySelf("Michael", "Callaway", 2016, 2024, "Red", "Nodle", "Brawl Stars"));
