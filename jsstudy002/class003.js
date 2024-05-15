2102d463fb741b4457b76d4eb08ff129f714db60
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-15 04:26:49 +0300

    New commit

--- jsstudy002/class003.js
+++ jsstudy002/class003.js
@@ -11,3 +11,26 @@ function randomNum(max) {
     return Math.floor(Math.random() * (max + 1));
 }
 console.log(randomNum(10));
+/*
+This method calculates the number of digits in a given string.
+Upon creation, the method takes a string parameter. It then
+iterates through each character in the string, checking whether
+it is a digit or not. If any character is determined to be a digit,
+the digit counter is incremented. As a result, the method returns
+the count of digits in the input string.
+*/
+/*
+This method calculates the number of digits in a given string. Upon creation, the method takes a string parameter. It then iterates through each character in the string, checking whether it is a digit or not. If any character is determined to be a digit, the digit counter is incremented. As a result, the method returns the count of digits in the input string.
+*/
+var countDigits = function (str) {
+    var digitCount = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (!isNaN(parseInt(str[i]))) {
+            digitCount++;
+        }
+    }
+    return digitCount;
+};
+// Example usage:
+var exampleString = "Hello123World456";
+console.log(countDigits(exampleString)); // Output: 6
