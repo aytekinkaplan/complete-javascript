734254390cb2fb40f598f411bb6169ad96cdcc50
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-05-17 10:31:03 +0300

    New commit

--- jsstudy004/class002.js
+++ jsstudy004/class002.js
@@ -1,19 +1,17 @@
-const jonas = {
+var jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
     birthYear: 1991,
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven'],
-    calcAge: function() {
+    calcAge: function () {
         console.log(this);
         console.log(2037 - this.birthYear);
     }
 };
-
 console.log(jonas.firstName);
 jonas.calcAge();
 console.log(jonas.lastName);
 console.log(jonas.birthYear);
 console.log(jonas.job);
-jonas.friends.forEach(friend => console.log(friend));
-
+jonas.friends.forEach(function (friend) { return console.log(friend); });
