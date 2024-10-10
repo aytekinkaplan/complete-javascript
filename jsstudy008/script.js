6c94eb4104450408cbcf875da7a614d46b6163af
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-10 10:12:20 +0300

    Up

--- jsstudy008/script.js
+++ jsstudy008/script.js
@@ -1,3 +1,9 @@
-// Creating DOM elements
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+
+const words = ["Hello", "World", "My", "Name", "is", "Aytekin", "Kaplan"];
+
+words.forEach((word, index) => {
+  setTimeout(() => {
+    myHeading.textContent = word;
+  }, index * 3000); // Her kelime için gecikme süresi artırılıyor
+});
