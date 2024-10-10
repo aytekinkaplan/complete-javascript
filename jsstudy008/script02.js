2602a19ffa2c61f06909c802509e5e6ef54918e5
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-10 15:55:48 +0300

    Up

--- jsstudy008/script02.js
+++ jsstudy008/script02.js
@@ -8,3 +8,17 @@
 // BMI (28.3) is higher than John's (23.9)!"
 // Hint: Use an if/else statement �
 // GOOD LUCK �
+
+const BMI = (mass, height) => {
+  return mass / (height * height);
+};
+
+const markBMI = BMI(78, 1.69);
+const johnBMI = BMI(92, 1.95);
+const markHigherBMI = markBMI > johnBMI;
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+  console.log("John's BMI (" + johnBMI + ") is higher than Mark's! (" + markBMI + ")");
+}
