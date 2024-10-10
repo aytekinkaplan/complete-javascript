49d888e9b683d2aa0209089620a7dbd50f196e2d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-10 15:56:28 +0300

    Up

--- jsstudy008/script02.js
+++ jsstudy008/script02.js
@@ -8,3 +8,9 @@
 // BMI (28.3) is higher than John's (23.9)!"
 // Hint: Use an if/else statement �
 // GOOD LUCK �
+
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+// m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+// m tall.
