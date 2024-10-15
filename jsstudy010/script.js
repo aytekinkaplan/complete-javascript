fbbcc246f68269f281f326a8fba58af07f093ad9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-15 22:58:05 +0300

    Up

---
+++ jsstudy010/script.js
@@ -0,0 +1,7 @@
+const element = document.querySelector(".rounded-edge");
+
+element.style.width = "500px";
+element.style.height = "900px";
+element.style.aspectRatio = "3/2";
+element.style.clipPath = "ellipse(85% 100% at 50% 0%)";
+element.style.backgroundColor = "#6c5ce7";
