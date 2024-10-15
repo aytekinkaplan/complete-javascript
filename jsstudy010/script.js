acc8afcf937d5f12ca13f549bd2e0912949dbd1e
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-16 00:39:57 +0300

    Up

--- jsstudy010/script.js
+++ jsstudy010/script.js
@@ -1,7 +1,18 @@
-const element = document.querySelector(".rounded-edge");
+const container = document.querySelector(".container");
 
-element.style.width = "500px";
-element.style.height = "900px";
-element.style.aspectRatio = "3/2";
-element.style.clipPath = "ellipse(85% 100% at 50% 0%)";
-element.style.backgroundColor = "#6c5ce7";
+// container flex ayarı
+container.style.display = "flex";
+container.style.justifyContent = "space-around"; // Öğeler arasında boşluk oluşturur (isteğe bağlı)
+container.style.alignItems = "center"; // Yatayda ortalar (isteğe bağlı)
+
+// Tüm .rounded-edge öğelerini seç
+const elements = document.querySelectorAll(".rounded-edge");
+
+// Döngü ile her bir öğeye stil uygula
+elements.forEach((element) => {
+  element.style.width = "500px";
+  element.style.height = "900px";
+  element.style.aspectRatio = "3 / 2";
+  element.style.clipPath = "ellipse(85% 100% at 50% 0%)";
+  element.style.backgroundColor = "#6c5ce7";
+});
